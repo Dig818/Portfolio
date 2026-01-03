98f11accbc5cbc11217f5ffb9c52c92415b5ffce
dig818 <digkhadka818@gmail.com>
2026-01-03 23:06:14 +0545

    Eight commit: Added animations and scroll-to-top button

--- src/script.js
+++ src/script.js
@@ -5,3 +5,36 @@ function toggleMenu() {
   menu.classList.toggle("open");
   icon.classList.toggle("open");
 }
+
+// SCROLL ANIMATION
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("active");
+    }
+  });
+});
+
+const hiddenElements = document.querySelectorAll(".reveal");
+hiddenElements.forEach((el) => observer.observe(el));
+
+// SCROLL TO TOP BUTTON
+const scrollUpBtn = document.getElementById("scroll-up-arrow");
+
+window.onscroll = function () {
+  if (
+    document.body.scrollTop > 300 ||
+    document.documentElement.scrollTop > 300
+  ) {
+    scrollUpBtn.classList.add("show");
+  } else {
+    scrollUpBtn.classList.remove("show");
+  }
+};
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
